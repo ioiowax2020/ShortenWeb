36b763bcda8564496c94a07cf6934a1e074b6d14
Kou <ioiowax2020.jp@gmail.com>
2021-05-21 23:56:15 +0900

    chore:heroku init

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = 'mongodb://localhost/shortenweb'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/shortenweb'
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
