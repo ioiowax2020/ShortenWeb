b231cf4c4c939ea428238bdc6fa4d561b2ba9d77
Kou <ioiowax2020.jp@gmail.com>
2021-05-22 01:17:58 +0900

    feat:fix index.js,shorten.js

--- routes/modules/shorten.js
+++ routes/modules/shorten.js
@@ -46,5 +46,20 @@ router.post('/show', (req, res) => {
     )
 })
 
+const mainURL = 'http://localhost:3000/shorten/'
 
+//redirect 
+router.get('/:shorten', (req, res) => {
+
+  const shortenName = req.params.shorten
+  console.log(shortenName)
+  Shortenweb.findOne({ name_short: shortenName })
+    .lean()
+    .then((web) => {
+      if (web) {
+        res.status(302).redirect(web.name)
+      }
+    })
+    .catch(error => console.log('error'))
+})
 module.exports = router
